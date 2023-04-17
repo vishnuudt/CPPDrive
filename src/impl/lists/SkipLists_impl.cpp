54a339b190f0c7f7353605fbacbe353e301dea4a
vishnu.udt <vishnu.udt@hotmail.com>
2023-04-16 17:38:25 -0700

    Compiling SkipLists

--- src/impl/lists/SkipLists_impl.cpp
+++ src/impl/lists/SkipLists_impl.cpp
@@ -2,7 +2,7 @@
 
 namespace Drive::CPP17::Lists{
 
-    SkipLists::SkipLists(){
+    SkipLists::SkipLists():n{}, h{}{
         std::cout << "cons SkipLists" << std::endl;
     }
 
@@ -30,6 +30,109 @@ namespace Drive::CPP17::Lists{
 
     void SkipLists::exportItem(){
         SkipLists skips;
+    }
+
+    string SkipLists::get(int i) {
+        return findPred(i)->next[0]->x;
+    }
+
+    string SkipLists::set(int i, string x) {
+        SkipListNode *u = findPred(i)->next[0];
+        string y = u->x;
+        u->x = x;
+        return y;
+    }
+
+    void SkipLists::add(int i, string x) {
+        SkipListNode *w = newNode(x, pickHeight());
+        if (w->height > h)
+        h = w->height;
+        add(i, w);
+    }
+
+   string SkipLists::remove(int i) {
+    string x;
+    SkipListNode *u = sentinel;
+    SkipListNode *del;
+    int r = h;
+    int j = -1; // index of node u
+    while (r >= 0) {
+        while (u->next[r] != NULL && j + u->length[r] < i) {
+            j += u->length[r];
+            u = u->next[r];
+        }
+        u->length[r]--; // for the node we are removing
+        if (j + u->length[r] + 1 == i && u->next[r] != NULL) {
+            x = u->next[r]->x;
+            u->length[r] += u->next[r]->length[r];
+            del = u->next[r];
+            u->next[r] = u->next[r]->next[r];
+            if (u == sentinel && u->next[r] == NULL)
+            h--;
+        }
+        r--;
+    }
+    deleteNode(del);
+    n--;
+    return x;
+  }
+
+  int SkipLists::pickHeight() {
+    int z = rand();
+    int k = 0;
+    int m = 1;
+    while ((z & m) != 0) {
+      k++;
+      m <<= 1;
+    }
+    return k;
+  }
+
+    SkipListNode* SkipLists::findPred(int i) {
+        SkipListNode *u = sentinel;
+        int r = h;
+        int j = -1;   // the index of the current node in list 0
+        while (r >= 0) {
+            while (u->next[r] != nullptr && j + u->length[r] < i) {
+                j += u->length[r];
+                u = u->next[r];
+            }
+            r--;
+        }
+        return u;
+    }
+
+    SkipListNode* SkipLists::add(int i, SkipListNode *w) {
+        SkipListNode *u = sentinel;
+        int k = w->height;
+        int r = h;
+        int j = -1; // index of u
+        while (r >= 0) {
+            while (u->next[r] != NULL && j + u->length[r] < i) {
+                j += u->length[r];
+                u = u->next[r];
+            }
+            u->length[r]++; // to account for new node in list 0
+            if (r <= k) {
+                w->next[r] = u->next[r];
+                u->next[r] = w;
+                w->length[r] = u->length[r] - (i - j);
+                u->length[r] = i - j;
+            }
+            r--;
+        }
+        n++;
+        return u;
+    }
+
+    SkipListNode* SkipLists::newNode(string x, int height){
+        SkipListNode* toreturn = new SkipListNode(x, height);
+        return toreturn;
+
+    }
+
+    void SkipLists::deleteNode(SkipListNode* todelete){
+        delete todelete;
     }
 
 }
--- src/include/lists/SkipLists.h
+++ src/include/lists/SkipLists.h
@@ -2,11 +2,28 @@
 
 #include <iostream>
 #include <memory>
+#include <string>
 
 using namespace std;
 
 namespace Drive::CPP17::Lists{
 
+
+struct SkipListNode {
+    string x;
+    int height;     // length of next
+    int *length;
+    SkipListNode **next;
+
+    SkipListNode(string input, int hei):x{input}, height{hei}{
+
+    }
+
+    ~SkipListNode(){
+        delete length;
+        delete [] next;
+    }
+};
 
 class SkipLists{
 
@@ -26,10 +43,27 @@ class SkipLists{
 
         static void exportItem();
 
-    private:
 
+        //functional interface
+        string get(int i);
+        string set(int i, string x);
+        void add(int i, string x);
+        string remove(int i);
+        
+
+    private:
+           int h;
+           int n;
+           SkipListNode* sentinel;
+           SkipListNode* findPred(int i);
+           SkipListNode* newNode(string x, int height);
+           SkipListNode* add(int i, SkipListNode *w);
+           void deleteNode(SkipListNode*);
+           int pickHeight();
 
 
 };
+
+
 
 }
